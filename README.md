00edbf9d3758b04e2e12766507a7b26fc0ff78ed
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2022-04-23 05:41:43 +0200

    add custom gnuradio things

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # radio-conf
 radio configuration running on the traffic stop boxes
+
+```
+nix-shell  -p '(gnuradio3_8.override { extraPackages = [ (callPackage ./reveng.nix {}) ]; })' -I nixpkgs=channel:nixos-21.05
+```
+
---
+++ reveng.nix
@@ -0,0 +1,37 @@
+{ stdenv, lib, fetchFromGitHub, cmake, pkgconfig, gnuradio3_8
+, makeWrapper, log4cpp, volk, icu
+, swig, gmp
+}:
+
+stdenv.mkDerivation {
+  pname = "gr-reveng";
+  version = "2021-05-22";
+
+  src = fetchFromGitHub {
+    owner = "paulgclark";
+    repo = "gr-reveng";
+    rev = "0a777f9ddb7e52e61cd68f9d95351ab26122a8bd";
+    sha256 = "15b39l6kx143ack9dimpsj5lyqlx19vxw5cax7yfv0nc8sahi0k5";
+  };
+
+  nativeBuildInputs = [ pkgconfig ];
+  buildInputs = [
+    cmake gnuradio3_8.unwrapped gnuradio3_8.unwrapped.boost gnuradio3_8.unwrapped.python makeWrapper log4cpp swig gmp volk icu
+  ];
+
+  postInstall = ''
+    for prog in "$out"/bin/*; do
+        wrapProgram "$prog" --set PYTHONPATH $PYTHONPATH:$(toPythonPath "$out")
+    done
+  '';
+
+  enableParallelBuilding = true;
+
+  meta = with lib; {
+    description = "Reverse Engineering module to gnuradio.";
+    homepage = https://github.com/paulgclark/gr-reveng;
+    license = licenses.gpl3Plus;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ marenz ];
+  };
+}
