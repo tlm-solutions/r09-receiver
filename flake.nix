6c20df5561456cafea252b70469371b2b0526897
Grigory Shipunov <blame@oxapentane.com>
2023-03-05 02:27:34 +0100

    pin nixpkgs to stable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Dump DVBAG public transport radio";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
 
     utils = {
       url = "github:numtide/flake-utils";
